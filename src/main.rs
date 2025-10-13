dc74659afb05f442693868bd8027a18286dc19e9
Spencer Salyer <spencer.salyer@proton.me>
2025-10-13 12:03:30 -0400

    victory!

--- src/main.rs
+++ src/main.rs
@@ -99,25 +99,54 @@ impl State {
         ctx.print_color_centered(9, GREEN, BLACK, "Press 1 to play again!");
 
         if let Some(VirtualKeyCode::Key1) = ctx.key {
-            self.ecs = World::default();
-            self.resources = Resources::default();
-
-            let mut rng = RandomNumberGenerator::new();
-            let map_builder = MapBuilder::new(&mut rng);
-
-            spawn_player(&mut self.ecs, map_builder.player_start);
-            spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
-            map_builder
-                .rooms
-                .iter()
-                .skip(1)
-                .map(|r| r.center())
-                .for_each(|pos| spawn_enemy(&mut self.ecs, &mut rng, pos));
-
-            self.resources.insert(map_builder.map);
-            self.resources.insert(Camera::new(map_builder.player_start));
-            self.resources.insert(TurnState::AwaitingInput);
+            self.reset()
         };
+    }
+
+    fn victory(&mut self, ctx: &mut BTerm) {
+        const DRAW_LAYER: usize = 2;
+
+        ctx.set_active_console(DRAW_LAYER);
+        ctx.print_color_centered(2, GREEN, BLACK, "You have won!@!");
+        ctx.print_color_centered(
+            4,
+            WHITE,
+            BLACK,
+            "You put on the Amulet of Yala and feel its power course through your veins...",
+        );
+        ctx.print_color_centered(
+            5,
+            WHITE,
+            BLACK,
+            "Your town is saved! You can return to your normal life!",
+        );
+        ctx.print_color_centered(7, GREEN, BLACK, "Press 1 to play again!");
+
+        if let Some(VirtualKeyCode::Key1) = ctx.key {
+            self.reset()
+        };
+    }
+
+    /// Reset the game state
+    fn reset(&mut self) {
+        self.ecs = World::default();
+        self.resources = Resources::default();
+
+        let mut rng = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut rng);
+
+        spawn_player(&mut self.ecs, map_builder.player_start);
+        spawn_amulet_of_yala(&mut self.ecs, map_builder.amulet_start);
+        map_builder
+            .rooms
+            .iter()
+            .skip(1)
+            .map(|r| r.center())
+            .for_each(|pos| spawn_enemy(&mut self.ecs, &mut rng, pos));
+
+        self.resources.insert(map_builder.map);
+        self.resources.insert(Camera::new(map_builder.player_start));
+        self.resources.insert(TurnState::AwaitingInput);
     }
 }
 
@@ -154,6 +183,7 @@ impl GameState for State {
                 .enemy_systems
                 .execute(&mut self.ecs, &mut self.resources),
             TurnState::GameOver => self.game_over(ctx),
+            TurnState::Victory => self.victory(ctx),
         }
 
         // render Draw buffer
